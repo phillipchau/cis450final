ba81f02fa49d0b2eb4a5f42eda7cbfa5f20475f1
Chris Elliott <elliott77chris@gmail.com>
2021-04-16 11:58:27 -0700

    Add server backend routes.

--- server/routes.js
+++ server/routes.js
@@ -23,7 +23,40 @@ function getVaccineData(req, res) {
   });
 }
 
+// Get the number of cases for each state for every date.
+function getDistinctStates(req, res) {
+  var query = `
+    SELECT DISTINCT State
+    FROM covid;
+  `;
+  connection.query(query, function(err, rows, fields) {
+    if (err) console.log(err);
+    else {
+      res.json(rows);
+    }
+  });
+}
+
+// Get the number of cases for each state for every date.
+function getCountPerStateDate(req, res) {
+  let type = 'CaseCount';
+  var query = `
+    SELECT Date, State, SUM(${type}) AS Cases
+    FROM covid
+    GROUP BY Date, State
+    ORDER BY Date ASC, State ASC;
+  `;
+  connection.query(query, function(err, rows, fields) {
+    if (err) console.log(err);
+    else {
+      res.json(rows);
+    }
+  });
+}
+
 // The exported functions, which can be accessed in index.js.
 module.exports = {
   getVaccineData: getVaccineData,
+  getDistinctStates: getDistinctStates,
+  getCountPerStateDate: getCountPerStateDate,
 }
